2d97a757954815d6a16d02afcd4e57104dc8cbb1
mic4ael <crossner90@gmail.com>
2015-12-27 18:29:22 +0100

    Display static data

--- custom/js/taskcalendar.js
+++ custom/js/taskcalendar.js
@@ -2,7 +2,25 @@ var vue = new Vue({
     el: '#container',
     computed: {
         days: function() {
-            return ['2014-01-01'];
+            return [{
+                title: 'Day 1',
+                tasks: [{
+                    title: 'Task 1',
+                    description: 'Description 1'
+                }, {
+                    title: 'Task 2',
+                    description: 'Description 2'
+                }]
+            }, {
+                title: 'Day 2',
+                tasks: [{
+                    title: 'Task 3',
+                    description: 'Description 3'
+                }, {
+                    title: 'Task 4',
+                    description: 'Description 4'
+                }]
+            }];
         }
     }
 });
